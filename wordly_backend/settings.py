6c89467a368913a77e829a043a7a8cec0814270e
Jim <jameslotruglio@gmail.com>
2024-10-08 18:51:19 -0400

    Completed heroku setup

--- wordly_backend/settings.py
+++ wordly_backend/settings.py
@@ -94,6 +94,8 @@ WSGI_APPLICATION = 'wordly_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -104,6 +106,8 @@ DATABASES = {
     }
 }
 
+import dj_database_url  
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
